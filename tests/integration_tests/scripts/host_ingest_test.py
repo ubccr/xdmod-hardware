220f0c98e13271d45eb1d032bd65512d79142348
Max Dudek <maxdudek@gmail.com>
2019-06-19 16:13:35 -0400

    Started a better testing system

--- tests/integration_tests/scripts/host_ingest_test.py
+++ tests/integration_tests/scripts/host_ingest_test.py
@@ -1,113 +1,72 @@
 import json
+import argparse
+import os
+import sys
+import time
+import datetime
 
 SECONDS_PER_DAY = 86400
-START_TIME = 1500000000
+HW_TYPES = []
+result = []
+currentTimeOf = {}
 
-def associate(host_num, hw_num, num_days):
+def associate(hostname, hw_num, num_days):
     """Associate a host with a hardware configuration
-    for a certain amount of time"""
-    end_time = currentTimeOf[host_num] + num_days*SECONDS_PER_DAY
-    for record_time_ts in range(currentTimeOf[host_num], end_time, SECONDS_PER_DAY):
-        newRow = [hosts[host_num]] + hw_types[hw_num] + [record_time_ts]
-        result.append(newRow)
-    currentTimeOf[host_num] = end_time
+    for a certain amount of time
+    If hw_num == -1, then associate the host with nothing
+    (i.e. simulate missing data for that number of days)"""
+    global HW_TYPES
+    global result
+    end_time = currentTimeOf[hostname] + num_days*SECONDS_PER_DAY
+    if hw_num != -1:
+        for record_time_ts in range(currentTimeOf[hostname], end_time, SECONDS_PER_DAY):
+            newRow = [hostname] + HW_TYPES[hw_num] + [record_time_ts] + ['test_resource']
+            result.append(newRow)
+    currentTimeOf[hostname] = end_time
 
+def getOptions():
+    """ process comandline options """
+    parser = argparse.ArgumentParser()
 
-result = [
-    [
-        'hostname',
-        'manufacturer',
-        'codename',
-        'model_name',
-        'clock_speed',
-        'core_count',
-        'board_manufacturer',
-        'board_name',
-        'board_version',
-        'system_manufacturer',
-        'system_name',
-        'system_version',
-        'physmem',
-        'numa_node_count',
-        'disk_count',
-        'ethernet_count',
-        'ib_device_count',
-        'ib_device',
-        'ib_ca_type',
-        'ib_ports',
-        'gpu_device_count',
-        'gpu_device_manufacturer',
-        'gpu_device_name',
-        'record_time_ts',
-    ]
-]
+    parser.add_argument('-c', '--config', help='Specify the path to the configuration directory', required=True)
 
-hosts = [
-    "cpn-k10-06-02",
-    "cpn-m28-18-02",
-]
+    parser.add_argument('-o', '--output', default='hardware_staging_test.json', help='Specify the name and path of the output json file')
 
-currentTimeOf = []
-for host_num in range(len(hosts)):
-    currentTimeOf.append(START_TIME)
+    args = parser.parse_args()
+    return vars(args)
 
-hw_types = [
-    [
-        "INTEL",
-        "Westmere EP",
-        "Intel(R) Xeon(R) CPU E5645",
-        "2.40GHz",
-        12,
-        "NA",
-        "NA",
-        "NA",
-        "Dell",
-        "C6100",
-        "NA",
-        48127,
-        2,
-        2,
-        3,
-        1,
-        "qib0",
-        "InfiniPath_QLE7340",
-        1,
-        0,
-        "NA",
-        "NA",
-    ], [
-        "INTEL",
-        "Ivy Bridge EP",
-        "Intel(R) Xeon(R) CPU E5-2650 v2",
-        "2.60GHz",
-        16,
-        "NA",
-        "NA",
-        "NA",
-        "HP",
-        "ProLiant SL230s Gen8",
-        "NA",
-        64223,
-        1,
-        1,
-        4,
-        1,
-        "mlx4_0",
-        "MT4099",
-        2,
-        0,
-        "NA",
-        "NA",
-    ]
-]
+def main():
+    global HW_TYPES
+    global result
 
-associate(0, 0, 7)
-associate(0, 1, 5)
-associate(0, 0, 10)
-associate(0, 0, 20)
+    opts = getOptions()
 
-associate(1, 1, 2)
-associate(1, 0, 10)
-associate(1, 1, 33)
+    outputFilename = opts['output']
 
-print(json.dumps(result, indent=4, separators=(',', ': ')))
+    confpath = opts['config']
+    conffile = os.path.join(confpath, "test_data.json")
+    with open(conffile, "r") as conffp:
+        try:
+            config = json.load(conffp)
+        except ValueError as exc:
+            raise Exception('Syntax error in %s.\n%s' % (conffile, str(exc)))
+
+    START_TIME = int(time.mktime(datetime.datetime.strptime(config['start_date'], "%Y-%m-%d").timetuple()))
+    result.append(config['column_names'])
+    HW_TYPES = config['hw_types']
+    ASSOCIATIONS = config['associations']
+
+    for hostname in ASSOCIATIONS:
+        currentTimeOf[hostname] = START_TIME
+        for association in ASSOCIATIONS[hostname]:
+            hw_num = association['hw_num']
+            num_days = association['num_days']
+            if hw_num >= len(HW_TYPES) or hw_num < -1:
+                raise IndexError('hw_num %d in config for host %s is illegal, %d hw_types were given in config' % (hw_num, hostname, len(HW_TYPES)))
+            associate(hostname, hw_num, num_days)
+
+    with open(outputFilename, 'w') as outFile:
+            outFile.write(json.dumps(result, indent=4, separators=(',', ': ')))
+
+if __name__ == '__main__':
+    main()
